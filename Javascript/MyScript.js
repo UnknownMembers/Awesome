04d631113992ac50170d1e65f0b92c11e63ab595
Vladislav <vlladislav45@gmail.com>
2018-10-19 11:43:30 +0300

    Add files via upload

--- Javascript/MyScript.js
+++ Javascript/MyScript.js
@@ -1,4 +1,26 @@
-var orderBtn;
+$(document).ready(() => {
+   var femaleBtn = document.getElementById("female-link");
+   var maleBtn = document.getElementById("male-link");
+   var womenBagsBtn = document.getElementById("womensbags-link");
+   var container = document.getElementsByClassName("container")
+   femaleBtn.addEventListener("click",function(){
+       $(container).load("FemaleShop.html")    
+   });  
+   
+   maleBtn.addEventListener("click",function(){
+         $(container).load("MenShoes.html")
+   });
+   
+   womenBagsBtn.addEventListener("click",function(){
+       $(container).load("WomensBags.html")
+   })
+   
+})
+
+
+
+
+/*var orderBtn;
 $(document).ready(() => {
    var femaleBtn = document.getElementById("female-link");
    var container = document.getElementsByClassName("container")
@@ -20,7 +42,8 @@ $(document).ready(() => {
    
     
   
-})
+}) */
+
 
 
 
