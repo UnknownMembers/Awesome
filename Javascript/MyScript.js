8e6568c44391dfa40b41e3fc96470828bf257bb2
Vladislav <vlladislav45@gmail.com>
2018-11-09 10:52:20 +0200

    Add files via upload
    Last update

--- Javascript/MyScript.js
+++ Javascript/MyScript.js
@@ -1,51 +1,76 @@
-$(document).ready(() => {
-   var femaleBtn = document.getElementById("female-link");
-   var maleBtn = document.getElementById("male-link");
-   var womenBagsBtn = document.getElementById("womensbags-link");
-   var container = document.getElementsByClassName("container")
-   femaleBtn.addEventListener("click",function(){
-       $(container).load("FemaleShop.html")    
-   });  
-   
-   maleBtn.addEventListener("click",function(){
-         $(container).load("MenShoes.html")
-   });
-   
-   womenBagsBtn.addEventListener("click",function(){
-       $(container).load("WomensBags.html")
-   })
-   
-})
+//global variables
+const BASE_URL = "https://baas.kinvey.com/";
+const APP_KEY = "kid_SyW6XSlaQ";
+const APP_SECRET = "61af6f9f690d4b3b9716f4f2423420c9";
+
+const HEADERS_BASIC = { 
+    "Authorization" : "Basic " + btoa(APP_KEY + ":" + APP_SECRET)
+};
+
+//     Facebook plugin
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/bg_BG/sdk.js#xfbml=1&version=v3.2';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+//    SlideShow
+
+var slideIndex = 0;
 
 
-
-
-/*var orderBtn;
-$(document).ready(() => {
-   var femaleBtn = document.getElementById("female-link");
-   var container = document.getElementsByClassName("container")
-   femaleBtn.addEventListener("click",function(){
-       $(container).load("FemaleShop.html")
-        orderBtn = document.getElementsByClassName("order")
-        for (btn of orderBtn) {
-            console.log($(btn))
-            $(btn).on("click", ()=> {
-                alert("")
-            })
-          btn.addEventListener("click",function(){
-             alert()
-              console.log(btn.parentNode)
-          })
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
     }
-   });
-   
-   
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
     
-  
-}) */
+    showSlides();
+}
 
+      //Register
+$(document).ready(function(){
+    
+    $(document).on("click", "#register", register);
+});
 
-
-
-
-
+function register(e){
+    e.preventDefault();
+    
+    let email = $("#reg-email").val();
+    let username = $("#reg-username").val();
+    let password = $("#reg-password").val();
+    let day = $("#reg-day").val();
+    let month = $("#reg-month").val();
+    let year = $("#reg-year").val();
+    
+    let data = {
+      email: email,
+      username: username,
+      password: btoa(password),
+      day: day,
+      month: month,
+      year: year
+    }; 
+    
+    $.ajax({
+        url: BASE_URL + "user/" + APP_KEY,
+        method: "POST",
+        headers: HEADERS_BASIC,
+        data: data
+    });
+}
