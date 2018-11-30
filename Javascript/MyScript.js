a2dd4edc2228f9e44bd1ec98309b5360162a372f
Vladislav <vlladislav45@gmail.com>
2018-11-30 11:28:46 +0200

    Add files via upload

--- Javascript/MyScript.js
+++ Javascript/MyScript.js
@@ -1,50 +1,51 @@
-//global variables
-const BASE_URL = "https://baas.kinvey.com/";
-const APP_KEY = "kid_SyW6XSlaQ";
-const APP_SECRET = "61af6f9f690d4b3b9716f4f2423420c9";
-
-const HEADERS_BASIC = { 
-    "Authorization" : "Basic " + btoa(APP_KEY + ":" + APP_SECRET)
-};
-
 //     Facebook plugin
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/bg_BG/sdk.js#xfbml=1&version=v3.2';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/bg_BG/sdk.js#xfbml=1&version=v3.2';
+                fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
 
-      //Register
-$(document).ready(function(){
-    
-    $(document).on("click", "#register", register);
+$(document).ready(function () {
+
+    var orderBtns = document.getElementsByClassName("view");
+
+    for (var i = 0; i < orderBtns.length; i++) {
+        orderBtns[i].addEventListener('click', openShoe, false);
+
+    }
 });
 
-function register(e){
-    e.preventDefault();
+function openShoe(event) {
+
+    var viewButton = event.target;
+    var divOrder = viewButton.parentNode;
+    // sibling can be used here 
+
+    var singleElement = divOrder.parentNode;
+    var childrenArray = singleElement.children;
+    for (var i = 0; i < childrenArray.length; i++) {
+        //Vzimame takushtiq element
+       var currentElement = childrenArray[i];
+//proverka dali e kartinka, zashtoto tam ne ni trqbva innerhtml, ami src-to (linka)
+        if (currentElement.tagName === "IMG") {
+           var imgSrc = currentElement.src;
+           
+       }
+        //ako e paragraph, trqbva ni cenata vytre
+        else if (currentElement.tagName === "P") {
+           var price = currentElement.innerHTML;
+//         console.log(currentElement);
+//console.log(currentElement.innerHtml);  
+        }
+        else if (currentElement.tagName === "H3")
+           var title = currentElement.innerHTML;
+        else
+            continue;
+         console.log(currentElement.tagName);
+    }
+  console.table([imgSrc, price, title]);
     
-    let email = $("#reg-email").val();
-    let username = $("#reg-username").val();
-    let password = $("#reg-password").val();
-    let day = $("#reg-day").val();
-    let month = $("#reg-month").val();
-    let year = $("#reg-year").val();
-    
-    let data = {
-      email: email,
-      username: username,
-      password: btoa(password),
-      day: day,
-      month: month,
-      year: year
-    }; 
-    
-    $.ajax({
-        url: BASE_URL + "user/" + APP_KEY,
-        method: "POST",
-        headers: HEADERS_BASIC,
-        data: data
-    });
 }
+
